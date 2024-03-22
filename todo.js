60aab7b641f20cb5c262bfa9a221e38f549ab9a2
[Maulik Bhavnagariya] <maulikbhavnagariya@gmail.com>
2024-03-22 16:43:46 +0530

    initial commit

---
+++ todo.js
@@ -0,0 +1,34 @@
+let todoArr = [];
+displayItems();
+
+
+function addTodo(){
+
+  let todoItems = document.querySelector('#todo-input');
+  let dateItems = document.querySelector('#todo-date');
+  let todoDatetext = dateItems.value;
+  let todoItemText = todoItems.value;
+  todoArr.push({item: todoItemText,duedate: todoDatetext});
+  todoItems.value = '';
+  dateItems.value = '';
+  localStorage.setItem('Item',JSON.stringify(todoArr));
+  displayItems();
+
+}
+
+function displayItems(){
+  
+  let containerElement = document.querySelector('#todo-container');
+  let newHtml = '';
+  for(let i = 0;i<todoArr.length;i++){
+    let {item,duedate} = todoArr[i];
+   newHtml += `
+      <span>${item}</span>
+      <span>${duedate}</span>
+      <button class="btn-delete" onclick="todoArr.splice(${i},1);
+      displayItems();    
+      ">Delete</button>
+    `;
+  }
+  containerElement.innerHTML = newHtml;
+}
